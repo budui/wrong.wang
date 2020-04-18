2115af6a089f22aab3db6c08de2837635d8b88cb
Wrong Wang <wangbudui@foxmail.com>
2020-04-18 16:07:45 +0800

    move all theme content into primer repo

--- themes/hugo-w2ng/assets/css/postcss.config.js
+++ themes/hugo-w2ng/assets/css/postcss.config.js
@@ -18,11 +18,7 @@ module.exports = {
         require('tailwindcss')(themeDir + 'assets/css/tailwind.config.js'),   
         require('@fullhuman/postcss-purgecss')({
             content: [themeDir + 'layouts/**/*.html'],
-            extractors: [
-            {
-                extractor: TailwindExtractor,
-                extensions: ['html']
-            }], 
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [], 
             fontFace: false
         }),    
         require('autoprefixer')({
