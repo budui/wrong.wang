82320de0f4e0720759ed2b543f2f8814ce5b82d5
Wrong Wang <wangbudui@foxmail.com>
2020-04-18 12:40:32 +0800

    copy theme from submodule

---
+++ themes/hugo-w2ng/README.md
@@ -0,0 +1,9 @@
+# hugo-w2ng
+
+`hugo-w2ng` is the theme for my personal [website](https://wrong.wang). It is made with `tailwindcss`, ported form [hugo-ivy](https://github.com/yihui/hugo-ivy/).
+
+## dev
+
+```bash
+npm install
+```
---
+++ themes/hugo-w2ng/assets/css/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      'sans': ['Optima', 'Candara', '"Lucida Sans"', 'Calibri', 'Arial',
+      'source-han-serif-sc', '"Source Han Serif SC"', '"Source Han Serif"',
+      '"Noto Serif CJK SC"', '"Noto Serif SC"',
+      '"Songti SC"', '"Microsoft YaHei"', "sans-serif"],
+      'mono': ['Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'SFMono-Regular', 'monospace'],
+    },
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+}
